2d49dcaa2800c8868856ea64a31d68b772762df7
RefiloeCollinMatabane <rcmatabane@gmail.com>
2021-10-24 05:08:14 +0200

    first commit

---
+++ src/components/pages/HomeCare.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HomeCare() {
+  return (
+    <>
+      <h1 className='HomeCare'>Home Care</h1>
+    </>
+  );
+}
---
+++ src/components/pages/PersonalCare.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PersonalCare() {
+  return (
+    <>
+      <h1 className='PersonalCare'>Personl Care</h1>
+    </>
+  );
+}
